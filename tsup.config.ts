af60f932243722a221cc162ac12ce5a2a982a447
Piotr Michalik <krzesaj.piotr.5@gmail.com>
2024-11-22 14:13:48 +0100

    fix: package.json publish cmd

--- tsup.config.ts
+++ tsup.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(overrideOptions => {
       errors: 'src/errors.ts',
     },
     dts: true,
-    onSuccess: shouldPublish ? 'pnpm publish:local' : undefined,
+    onSuccess: shouldPublish ? 'pnpm publish' : undefined,
     format: ['cjs', 'esm'],
     bundle: true,
     clean: true,
