a1a92660ebf6c93cf86fd56f6fc81b5b25480f5e
Piotr Michalik <krzesaj.piotr.5@gmail.com>
2024-12-02 17:37:51 +0100

    feat: add custom backend host method

--- src/backend.ts
+++ src/backend.ts
@@ -1,72 +1,90 @@
-import { BitsnapModels } from "./models"
+import { BitsnapModels } from "./models";
+
+let BACKEND_HOST = "https://bitsnap.pl";
 
 export namespace BitsnapBackend {
+  export async function setCustomHost(host: string) {
+    BACKEND_HOST = host;
+  }
+
   export async function getProduct(projectID: string, id: string) {
     const payload = {
-      '0': {
+      "0": {
         projectID: projectID,
         id: id,
       },
-    }
+    };
 
-    const encodedPayload = new URLSearchParams()
-    encodedPayload.set('batch', '1')
-    encodedPayload.set('input', JSON.stringify(payload))
+    const encodedPayload = new URLSearchParams();
+    encodedPayload.set("batch", "1");
+    encodedPayload.set("input", JSON.stringify(payload));
 
     const result = await fetch(
-      'https://bitsnap.pl/api/trpc/product.getProductById?' + encodedPayload.toString(),
+      BACKEND_HOST +
+        "/api/trpc/product.getProductById?" +
+        encodedPayload.toString(),
       {
         headers: {
-          'Content-Type': 'application/json',
-          Priority: 'u=3, i',
+          "Content-Type": "application/json",
+          Priority: "u=3, i",
         },
       },
-    )
+    );
 
-    const downloadedPayload = await result.json()
+    const downloadedPayload = await result.json();
 
-    const parsedResult = await BitsnapModels.ProductResultSchema.parseAsync(downloadedPayload)
+    const parsedResult =
+      await BitsnapModels.ProductResultSchema.parseAsync(downloadedPayload);
 
     if (parsedResult.length == 0) {
-      return undefined
+      return undefined;
     }
-    const parsed = parsedResult[0]
+    const parsed = parsedResult[0];
 
     return parsed.result;
   }
 
-  export async function getProducts(projectID: string, limit: number, offset: number) {
+  export async function getProducts(
+    projectID: string,
+    limit: number,
+    offset: number,
+  ) {
     const productsPayload = {
-      '0': {
+      "0": {
         projectID: projectID,
         limit: limit,
         offset: offset,
       },
-    }
-    const encodedPayload = new URLSearchParams()
-    encodedPayload.set('batch', '1')
-    encodedPayload.set('input', JSON.stringify(productsPayload))
+    };
+    const encodedPayload = new URLSearchParams();
+    encodedPayload.set("batch", "1");
+    encodedPayload.set("input", JSON.stringify(productsPayload));
 
     const result = await fetch(
-      'https://bitsnap.pl/api/trpc/product.getProductGrid?' + encodedPayload.toString(),
+      BACKEND_HOST +
+        "/api/trpc/product.getProductGrid?" +
+        encodedPayload.toString(),
       {
         headers: {
-          'Content-Type': 'application/json',
-          Priority: 'u=3, i',
+          "Content-Type": "application/json",
+          Priority: "u=3, i",
         },
       },
-    )
+    );
 
-    const downloadedPayload = await result.json()
+    const downloadedPayload = await result.json();
 
-    const parsedResult = await BitsnapModels.ProductsResultElementSchema.parseAsync(downloadedPayload)
+    const parsedResult =
+      await BitsnapModels.ProductsResultElementSchema.parseAsync(
+        downloadedPayload,
+      );
     if (parsedResult.length == 0) {
       return {
         categories: undefined,
         products: undefined,
-      }
+      };
     }
-    const parsed = parsedResult[0]
+    const parsed = parsedResult[0];
 
     return parsed.result;
   }
