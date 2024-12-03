07fa9558535bd9574d89ca7f5ae80b1edbfc7dec
Piotr Michalik <krzesaj.piotr.5@gmail.com>
2024-12-03 13:21:35 +0100

    fix: description field is optional

--- src/models.ts
+++ src/models.ts
@@ -1,12 +1,12 @@
-import * as z from 'zod'
+import * as z from "zod";
 
 export namespace BitsnapModels {
   export const BaselinkerFieldsSchema = z.object({
     storageID: z.string(),
-  })
-  export type BaselinkerFields = z.infer<typeof BaselinkerFieldsSchema>
+  });
+  export type BaselinkerFields = z.infer<typeof BaselinkerFieldsSchema>;
 
-  export const MetadataSchema = z.record(z.string(), z.any())
+  export const MetadataSchema = z.record(z.string(), z.any());
 
   export const VariantSchema = z.object({
     id: z.string(),
@@ -16,20 +16,20 @@ export namespace BitsnapModels {
     metadata: MetadataSchema,
     images: z.array(z.string()),
     availableQuantity: z.number(),
-  })
-  export type Variant = z.infer<typeof VariantSchema>
+  });
+  export type Variant = z.infer<typeof VariantSchema>;
 
   export const AdditionalSchema = z.object({
     sku: z.string(),
     baselinkerFields: BaselinkerFieldsSchema,
-  })
-  export type Additional = z.infer<typeof AdditionalSchema>
+  });
+  export type Additional = z.infer<typeof AdditionalSchema>;
 
   export const ItemSchema = z.object({
     id: z.string(),
     ownerID: z.string(),
     name: z.string(),
-    description: z.string(),
+    description: z.union([z.string(), z.null()]).optional(),
     createdAt: z.number(),
     updatedAt: z.number(),
     price: z.number(),
@@ -42,27 +42,29 @@ export namespace BitsnapModels {
     additional: AdditionalSchema,
     deletedAt: z.null(),
     variants: z.array(VariantSchema),
-  })
-  export type Item = z.infer<typeof ItemSchema>
+  });
+  export type Item = z.infer<typeof ItemSchema>;
 
   export const DataSchema = z.object({
     success: z.boolean(),
     result: z.array(ItemSchema),
     totalCount: z.number(),
-  })
-  export type Data = z.infer<typeof DataSchema>
+  });
+  export type Data = z.infer<typeof DataSchema>;
 
   export const ProductsResultResultSchema = z.object({
     data: DataSchema,
-  })
-  export type ProductsResultResult = z.infer<typeof ProductsResultResultSchema>
+  });
+  export type ProductsResultResult = z.infer<typeof ProductsResultResultSchema>;
 
   export const ProductsResultElementSchema = z.array(
     z.object({
       result: ProductsResultResultSchema,
     }),
-  )
-  export type ProductsResultElement = z.infer<typeof ProductsResultElementSchema>
+  );
+  export type ProductsResultElement = z.infer<
+    typeof ProductsResultElementSchema
+  >;
 
   export const ProductResultSchema = z.array(
     z.object({
@@ -74,5 +76,5 @@ export namespace BitsnapModels {
         }),
       }),
     }),
-  )
+  );
 }
