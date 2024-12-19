e13c87bff6e18469a654489e00870aec761b26f4
Piotr Michalik <krzesaj.piotr.5@gmail.com>
2024-12-19 11:27:36 +0100

    fix: export LinkRequest type

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export { default as BitsnapCheckout } from './checkout/BitsnapCart';
 export { setProjectID } from './checkout/CartProvider';
 export { setCustomHost } from './checkout/constants';
+export { type LinkRequest } from "./checkout/link.request.schema";
 export * from "./checkout/methods";
 export * from './webhook.handler';
 
